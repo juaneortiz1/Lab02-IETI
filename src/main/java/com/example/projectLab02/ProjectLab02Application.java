7db1d0845f2d1257764fb09452612147624cedd8
Juanes <juan.ortiz-p@mail.escuelaing.edu.co>
2024-08-24 17:39:52 -0500

    First Commit

---
+++ src/main/java/com/example/projectLab02/ProjectLab02Application.java
@@ -0,0 +1,13 @@
+package com.example.projectLab02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLab02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLab02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectLab02/ProjectLab02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectLab02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLab02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
